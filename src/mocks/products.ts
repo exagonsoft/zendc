92ef5cdea65d2ee74ff5690d8730e6e6cbe74f40
melkorAMP <melkor@aleph.engineering>
2024-06-09 21:31:23 -0400

    #7 Add Mock Products

---
+++ src/mocks/products.ts
@@ -0,0 +1,50 @@
+export const featuredProducts = [
+    {
+        title: `Paris Print, Men's Graphic Design Crew Neck T-shirt, Casual Comfy Tees Tshirts For Summer, Men's Clothing Tops For Daily Vacation Resorts`,
+        image: '/products/ed0bd2f9-7f61-48e7-9406-2aaa2759a27e.jpg',
+        price: 6.07,
+        rate: 5
+    },
+    {
+        title: `Men's Outfit, Color Matching King Pattern Casual Crew Neck Short Sleeve T-Shirt & Drawstring Shorts 2-piece Set For Summer Outdoor Activities`,
+        image: '/products/549a85c8-41fe-420e-8def-7b524e516814.jpg',
+        price: 7.09,
+        rate: 4
+    },
+    {
+        title: `2-piece Men's Anchor And Stripe Pattern Outfit Set, Men's Color Block Short Sleeve Crew Neck T-shirt & Drawstring Shorts Set For Summer Outdoor`,
+        image: '/products/bb5da3b9-cc50-4884-aca4-20ec8c1a9236.jpg',
+        price: 14.37,
+        rate: 4
+    },
+    {
+        title: `Men's Breathable Color Block Golf Shirt, Men's Casual Short Sleeve Button Up Lapel T-Shirt For Summer, Men's Clothing`,
+        image: '/products/bd4eeceb-d05f-4fa8-a26c-167744371f34.jpg',
+        price: 7.79,
+        rate: 4
+    },
+    {
+        title: `Creative Letter Print, Men's Graphic Design Crew Neck T-shirt, Casual Comfy Tees For Summer, Men's Clothing Tops For Daily Gym Workout Running`,
+        image: '/products/69e86b1a-c79c-468e-8b02-6c2eda048ebc.jpg',
+        price: 6.90,
+        rate: 4
+    },
+    {
+        title: `Men's Outfit, Gradient Color Whatever Graphic Print Casual Crew Neck Short Sleeve T-Shirt & Drawstring Shorts 2-piece Set For Summer Outdoor Activities`,
+        image: '/products/1e550f58-3b9f-4507-aaba-2ca5e39b9f7d.jpg',
+        price: 15.11,
+        rate: 5
+    },
+    {
+        title: `Y2k Hip Hop Style Raw Trim Jeans, Men's Casual Street Style Stretch Denim Pants`,
+        image: '/products/a8bcd2bf-2884-4c31-b4ec-2d4c9b708045.jpg',
+        price: 22.85,
+        rate: 4
+    },
+    {
+        title: `Whatever Handwritten Letter Print Men's Daily Short Sleeve Crew Neck T-shirt, Summer Outdoor`,
+        image: '/products/a466f038-2cab-4999-aebb-9ed7d3de106b.jpg',
+        price: 3.29,
+        rate: 5
+    },
+]
